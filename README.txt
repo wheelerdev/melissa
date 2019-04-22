c5c37859b488c79e61069daa3129f736cdc8a45f
ghostcamp <thefinalsound@gmail.com>
2019-04-22 14:01:26 -0700

    Starting Point
    Main page mostly finished. Mobile nav working. Beginning to build work sample pages.

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mxbck/pen/xdaGNL.
+
+ Accessible, Progressive-Enhanced Navigation Menu with a circular animated background.
---
+++ js/index.js
@@ -0,0 +1,40 @@
+var nav = document.querySelector('#nav');
+var menu = document.querySelector('#menu');
+var menuToggle = document.querySelector('.nav__toggle');
+var isMenuOpen = false;
+
+
+// TOGGLE MENU ACTIVE STATE
+menuToggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  isMenuOpen = !isMenuOpen;
+
+  // toggle a11y attributes and active class
+  menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
+  menu.hidden = !isMenuOpen;
+  nav.classList.toggle('nav--open');
+});
+
+
+// TRAP TAB INSIDE NAV WHEN OPEN
+nav.addEventListener('keydown', function (e) {
+  // abort if menu isn't open or modifier keys are pressed
+  if (!isMenuOpen || e.ctrlKey || e.metaKey || e.altKey) {
+    return;
+  }
+
+  // listen for tab press and move focus
+  // if we're on either end of the navigation
+  var menuLinks = menu.querySelectorAll('.nav__link');
+  if (e.keyCode === 9) {
+    if (e.shiftKey) {
+      if (document.activeElement === menuLinks[0]) {
+        menuToggle.focus();
+        e.preventDefault();
+      }
+    } else if (document.activeElement === menuToggle) {
+      menuLinks[0].focus();
+      e.preventDefault();
+    }
+  }
+});
